f2edb3d7e0f52183dd2e141ea9183991c5e63078
Nikola Kubeczkova <nikola.kubeczkova@olc.cz>
2025-09-04 21:08:50 +0200

    :construction: little progress

---
+++ src/lib/components/landing.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { t } from '$lib/i18n';
+	import { ChevronDown } from '@lucide/svelte';
+
+</script>
+
+<section>
+	<h1>{$t('landing.name')}</h1>
+	<h3>{$t('landing.tagline')}</h3>
+	<p>{$t('landing.intro')}</p>
+	<h3>
+		<ChevronDown size={30} />
+		{$t('landing.call-to-action')}
+		<ChevronDown size={30} />
+</h3>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
 	import { t } from '$lib/i18n';
-	import { ChevronDown } from '@lucide/svelte';
 
 	import Projects from '$lib/components/projects.svelte';
 	import Skills from '$lib/components/skills.svelte';
 	import Contact from '$lib/components/contact.svelte';
 	import Education from '$lib/components/education.svelte';
 	import Development from '$lib/components/development.svelte';
+	import Landing from '\$lib/components/landing.svelte';
 
 	export let data;
 </script>
 
 <svelte:head>
 	<title>{$t('menu.home')}</title>
-	<meta name="description" content="Nikola Kubeczková" />
+	<meta />
 </svelte:head>
 
-<section>
-	<h1>{$t('landing.name')}</h1>
-	<h3>{$t('landing.tagline')}</h3>
-	<p>{$t('landing.intro')}</p>
-	<h3>
-		<ChevronDown size={30} />
-		{$t('landing.call-to-action')}
-		<ChevronDown size={30} />
-</h3>
-</section>
+<Landing />
 
 <Skills data={data}/>
 
@@ -36,14 +27,3 @@
 <Education />
 
 <Contact />
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-</style>
